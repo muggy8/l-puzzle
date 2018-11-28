782f1051a7a1429bacfa38d807b0965c3823f744
muggy8 <muggy8@gmail.com>
2018-11-27 21:56:23 -0800

    refactored the findLittleLMoves to be a more general solution to find any L moves given the movement map avalable

--- index.js
+++ index.js
@@ -23,8 +23,18 @@ const littleLMoves = [
 	[-1,1],
 	[-1,-1],
 ]
-function findLittleLMoves(currentPosition){
-	let viableMoves = littleLMoves.map(function(position){
+const bigMoves = [
+	[1,2],
+	[1,-2],
+	[-1,2],
+	[-1,-2],
+	[2,1],
+	[2,-1],
+	[-2,1],
+	[-2,-1],
+]
+function findLMoves(currentPosition, possiableMoves){
+	let viableMoves = possiableMoves.map(function(position){
 		var newPosition = [position[0] + currentPosition[0], position[1] + currentPosition[1]]
 
 		if (newPosition[0] < 0 || newPosition[0] > 3 || newPosition[1] < 0 || newPosition[1] > 2 ){ // early break point to get out of anything that isn't within the bounds
